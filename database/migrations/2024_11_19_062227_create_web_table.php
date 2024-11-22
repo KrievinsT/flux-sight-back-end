e03badc26ad857f1b69c3b9ebbe15570725bcaff
Dragon <115075047+Dragonbestgg1@users.noreply.github.com>
2024-11-22 11:53:17 +0200

    migration update

--- database/migrations/2024_11_19_062227_create_web_table.php
+++ database/migrations/2024_11_19_062227_create_web_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('web', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->string('url');
             $table->string('seo');
             $table->float('page_speed');
